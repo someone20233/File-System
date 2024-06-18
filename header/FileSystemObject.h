80582939c0b8447faecd9f2cf5217fd16c7a2f3a
someone20233 <ak2476@live.mdx.ac.uk>
2024-06-18 20:00:15 +0500

    Changes in base class.
    Namespace modified. Added Implementation for the functions in the cpp file.

--- header/FileSystemObject.h
+++ header/FileSystemObject.h
@@ -4,23 +4,23 @@
 #include <string>
 #include <chrono>
 using namespace std;
+using namespace chrono;
 
-// Parent Class for the File and Directory classes
-// Contains the name and last modified time of the object
+// Base class for all filesystem objects
 class FileSystemObject {
 protected:
     string name;    // Name of the object
-    chrono::system_clock::time_point lastModified;  // Last modified time of the object
+    system_clock::time_point lastModified;  // Last modified time of the object
 
 public:
     // Constructor
-    FileSystemObject(const std::string& name);
+    FileSystemObject(const string& name);
     // Destructor
     virtual ~FileSystemObject() = default;
     // Getters and Setters
     string getName() const;
     chrono::system_clock::time_point getLastModified() const;
-    void setLastModified(std::chrono::system_clock::time_point time);
+    void setLastModified(system_clock::time_point time);
 };
 
 #endif // FILESYSTEMOBJECT_H
---
+++ source/FileSystemObject.cpp
@@ -0,0 +1,19 @@
+#include "../header/FileSystemObject.h"
+
+// Constructor for FileSystemObject
+FileSystemObject::FileSystemObject(const string& name): name(name), lastModified(system_clock::now()) {
+    // Nothing to do here
+}
+
+// Getters and Setters
+string FileSystemObject::getName() const {
+    return name;
+}
+
+system_clock::time_point FileSystemObject::getLastModified() const {
+    return lastModified;
+}
+
+void FileSystemObject::setLastModified(system_clock::time_point time) {
+    lastModified = time;
+}
