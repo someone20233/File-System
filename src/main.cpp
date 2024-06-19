b9e36bda6a535b448a7fadda7f977fb46ebe402f
someone20233 <ak2476@live.mdx.ac.uk>
2024-06-19 15:31:13 +0500

    Update Main.
    CLI Application.

--- src/main.cpp
+++ src/main.cpp
@@ -1,58 +1,112 @@
-#include "../include/FileSystemManager.h"
 #include <iostream>
 #include <string>
 #include <iomanip>
+#include "../include/FileSystemManager.h"
 
 using namespace std;
 
+void showMenu(const string& currentDirectory) {
+    cout << "Filesystem CLI - Current Directory: " << currentDirectory << "\n";
+    cout << "1. Create File\n";
+    cout << "2. Delete File\n";
+    cout << "3. Write to File\n";
+    cout << "4. Read from File\n";
+    cout << "5. Create Directory\n";
+    cout << "6. List Directory\n";
+    cout << "7. Change Directory\n";
+    cout << "8. Go Back\n";
+    cout << "9. Delete Directory\n";
+    cout << "10. Exit\n";
+    cout << "Enter your choice: ";
+}
+
 int main() {
     FileSystemManager fs;
-    string command;
-
-    cout << "Simple Filesystem CLI" << endl;
+    int choice;
+    string name, content;
+    size_t size;
+    
     while (true) {
-        cout << "Current Directory: " << fs.getCurrentDirectoryPath() << " > ";
-        getline(cin, command);
-
-        if (command == "exit") break;
+        showMenu(fs.getCurrentDirectoryPath());
+        cin >> choice;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n'); // flush input
+        
         try {
-            if (command.compare(0, 12, "create file ") == 0) {
-                fs.createFile(command.substr(12));
-            } else if (command.compare(0, 12, "delete file ") == 0) {
-                fs.deleteFile(command.substr(12));
-            } else if (command.compare(0, 10, "open file ") == 0) {
-                auto fd = fs.openFile(command.substr(10));
-                cout << "Opened file: " << fd->getFile()->getName() << endl;
-            } else if (command.compare(0, 6, "write ") == 0) {
-                string name = command.substr(6, command.find(' ', 6) - 6);
-                string content = command.substr(command.find(' ', 6) + 1);
-                auto fd = fs.openFile(name);
-                fs.writeFile(fd, content);
-                fs.closeFile(fd);
-            } else if (command.compare(0, 5, "read ") == 0) {
-                auto fd = fs.openFile(command.substr(5));
-                cout << fs.readFile(fd, 1024) << endl; // Read up to 1024 bytes
-                fs.closeFile(fd);
-            } else if (command.compare(0, 11, "create dir ") == 0) {
-                fs.createDirectory(command.substr(11));
-            } else if (command.compare(0, 11, "delete dir ") == 0) {
-                fs.deleteDirectory(command.substr(11));
-            } else if (command == "list") {
-                auto contents = fs.listDirectoryWithDetails();
-                for (const auto& [name, size, modified] : contents) {
-                    cout << setw(20) << name << setw(10) << size << setw(20) << modified << endl;
-                }
-            } else if (command.compare(0, 3, "cd ") == 0) {
-                fs.changeDirectory(command.substr(3));
-            } else if (command == "back") {
-                fs.goBack();
-            } else {
-                cout << "Unknown command: " << command << endl;
+            switch (choice) {
+                case 1:
+                    cout << "Enter file name (with extension): ";
+                    getline(cin, name);
+                    fs.createFile(name);
+                    cout << "File created.\n";
+                    break;
+                case 2:
+                    cout << "Enter file name: ";
+                    getline(cin, name);
+                    fs.deleteFile(name);
+                    cout << "File deleted.\n";
+                    break;
+                case 3:
+                    cout << "Enter file name: ";
+                    getline(cin, name);
+                    if (FileDescriptor* fd = fs.openFile(name)) {
+                        cout << "Enter content to write: ";
+                        getline(cin, content);
+                        fs.writeFile(fd, content);
+                        fs.closeFile(fd);
+                        cout << "File written.\n";
+                    } else {
+                        cout << "File not found.\n";
+                    }
+                    break;
+                case 4:
+                    cout << "Enter file name: ";
+                    getline(cin, name);
+                    cout << "Enter number of bytes to read: ";
+                    cin >> size;
+                    cin.ignore(numeric_limits<streamsize>::max(), '\n'); // flush input
+                    if (FileDescriptor* fd = fs.openFile(name)) {
+                        string data = fs.readFile(fd, size);
+                        fs.closeFile(fd);
+                        cout << "Data read from file: " << data << "\n";
+                    } else {
+                        cout << "File not found.\n";
+                    }
+                    break;
+                case 5:
+                    cout << "Enter directory name: ";
+                    getline(cin, name);
+                    fs.createDirectory(name);
+                    cout << "Directory created.\n";
+                    break;
+                case 6:
+                    for (const auto& [entry, size, date] : fs.listDirectoryWithDetails()) {
+                        cout << setw(20) << left << entry 
+                                  << setw(10) << right << size 
+                                  << setw(25) << right << date << "\n";
+                    }
+                    break;
+                case 7:
+                    cout << "Enter directory path: ";
+                    getline(cin, name);
+                    fs.changeDirectory(name);
+                    break;
+                case 8:
+                    fs.goBack();
+                    break;
+                case 9:
+                    cout << "Enter directory name: ";
+                    getline(cin, name);
+                    fs.deleteDirectory(name);
+                    cout << "Directory deleted.\n";
+                    break;
+                case 10:
+                    cout << "Exiting.\n";
+                    return 0;
+                default:
+                    cout << "Invalid choice. Try again.\n";
             }
         } catch (const exception& e) {
-            cout << "Error: " << e.what() << endl;
+            cerr << e.what() << "\n";
         }
     }
-
-    return 0;
 }
